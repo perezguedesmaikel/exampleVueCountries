5d648ca5b1d3ebabc6a94c7cc47e3551e762407e
perezguedesmaikel <perezguedesmaikel@gmail.com>
2023-11-12 17:14:48 -0500

    all countries

---
+++ src/components/CountryItem.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import {IData} from "./interfaces.ts";
+
+interface props {
+  el: IData
+  index: string
+}
+
+defineProps<props>()
+</script>
+
+<template>
+  <div class="h-56 sm:h-36">
+    <img :alt="el.name.common" :src="el.flags.png" class="h-56 sm:h-36 w-full"/>
+  </div>
+  <div class="flex flex-col justify-center items-start">
+    <h4 class="text-xl font-bold text-gray-500">{{ index + el.name.common }}</h4>
+    <p class="text-sm text-gray-400">Capital:<span v-for="element in el.capital" class="font-bold">{{ element }}
+      <template> </template></span>
+    </p>
+    <p class="text-sm text-gray-400">Population:<span class="font-bold">{{ el.population?.toLocaleString() }}</span></p>
+    <p class="text-sm text-gray-400">Región:<span class="font-bold">{{ el.region?.toLocaleString() }}</span></p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/components/interfaces.ts
+++ src/components/interfaces.ts
@@ -1,6 +1,15 @@
 export interface IData{
     name:{common:string}
+    capital:string[],
+    cca3:string,
+    flags:{
+        alt:string,
+        png:string
+    }
+    population:number,
+    region:string
 }
 export interface ICountries{
-    country:IData[]
+    country:IData[],
+
 }
